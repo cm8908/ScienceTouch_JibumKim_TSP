60766681355dc742645aa317bd2474028a0d1074
cm8908 <9781689+cm8908@users.noreply.github.com>
2023-10-16 18:02:37 +0900

    dependencies into one file

--- Transformer-TSP50/my_model_packages.py
+++ Transformer-TSP50/my_model_packages.py
@@ -1,7 +1,6 @@
 import torch
 from torch import nn
 from torch.distributions.categorical import Categorical
-from conv_embedding import ConvEmbedding, ConvEmbeddingXY, ConvSamePadding, ConvLinear
 
 class Transformer_encoder_net(nn.Module):
     """
@@ -512,3 +511,154 @@ class TSP_net(nn.Module):
             scores_beamsearch = sum_scores
 
         return tours_greedy, tours_beamsearch, scores_greedy, scores_beamsearch
+
+class ConvEmbedding(nn.Module):
+    def __init__(self, nb_neighbors, kernel_size, dim_emb, dim_input_nodes):
+        super().__init__()
+        self.nb_neighbors = nb_neighbors
+        self.conv = nn.Conv1d(in_channels=dim_input_nodes, out_channels=dim_emb, kernel_size=kernel_size)
+        self.W1 = nn.Linear(dim_input_nodes, dim_emb)  # for node x_i
+        self.W2 = nn.Linear(dim_emb, dim_emb)  # for convolved node feature hbar_i
+
+    def forward(self, x):
+        """
+        :param Tensor x: (B, N, 2)
+        :return Tensor final_embedding: (B, N, H)
+        """
+        bsz, seq_len = x.size(0), x.size(1)
+
+        node_embedding = self.W1(x)  # (B, N, H)
+
+        # Make k-NN for each node (B, N, K+1, 2)
+        dist_matrix = torch.cdist(x, x)  # (B, N, N)
+        # knn_indices = dist_matrix.topk(self.nb_neighbors+1)[1]  # (B, N, K+1) including itself
+        knn_indices = dist_matrix.topk(k=seq_len)[1][:,:,-self.nb_neighbors-1:]  # (B, N, K+1) including itself
+        embedding_list = []
+        for i in range(seq_len):
+            idx = knn_indices[:, i, :].unsqueeze(2).repeat(1,1,2)  # (B, K+1, 2)
+            knn_coords = x.gather(1, idx)
+
+            knn_coords = knn_coords.permute(0, 2, 1)  # (B, 2, K+1)
+            conv_embedding = self.conv(knn_coords)  # (B, H, 1)
+            
+            conv_embedding = conv_embedding.permute(0, 2, 1)  # (B, 1, H)
+            # conv_embedding = self.W2(conv_embedding)  # (B, 1, H)  comment
+
+            embedding_list.append(conv_embedding)
+        conv_embedding = torch.cat(embedding_list, dim=1)  # (B, N, H)
+        conv_embedding = self.W2(conv_embedding)  # (B, N, H)  namely CEFix
+
+        final_embedding = node_embedding + conv_embedding  # (B, N, H)
+        return final_embedding
+
+class ConvSamePadding(nn.Module):
+    def __init__(self, dim_input_nodes, dim_emb, kernel_size):
+        super().__init__()
+        self.conv = nn.Conv1d(in_channels=dim_input_nodes, out_channels=dim_emb, kernel_size=kernel_size, padding='same')
+    def forward(self, x):
+        """(B, N, 2) --> (B, N, H)"""
+        x = x.permute(0,2,1)  # (B, 2, N)
+        h = self.conv(x)  # (B, H, N)
+        h = h.permute(0, 2, 1)  # (B, N, H)
+        return h
+
+class ConvLinear(nn.Module):
+    def __init__(self, dim_input_nodes, dim_emb, kernel_size):
+        super().__init__()
+        self.conv = nn.Conv1d(in_channels=dim_input_nodes, out_channels=dim_emb, kernel_size=kernel_size, padding='same')
+        self.W1 = nn.Linear(dim_input_nodes, dim_emb)
+        self.W2 = nn.Linear(dim_emb, dim_emb)
+    def forward(self, x):
+        """(B, N, 2) --> (B, N, H)"""
+        node_embedding = self.W1(x)  # (B, N, H)
+        x = x.permute(0, 2, 1)  # (B, 2, N)
+        conv_embedding = self.conv(x)  # (B, H, N)
+        conv_embedding = conv_embedding.permute(0, 2, 1)  # (B, N, H)
+        final_embedding = node_embedding + conv_embedding
+        return final_embedding
+
+class ConvEmbeddingXY(nn.Module):
+    def __init__(self, nb_neighbors, kernel_size, dim_emb, dim_input_nodes):
+        super().__init__()
+        self.nb_neighbors = nb_neighbors
+        self.conv_x = nn.Conv1d(in_channels=dim_input_nodes, out_channels=dim_emb, kernel_size=kernel_size)
+        self.conv_y = nn.Conv1d(in_channels=dim_input_nodes, out_channels=dim_emb, kernel_size=kernel_size)
+        self.W1 = nn.Linear(dim_input_nodes, dim_emb)  # for node x_i
+        self.W2 = nn.Linear(dim_emb, dim_emb)  # for convolved node feature hbar_i
+    
+    def _sort_by_xy(self, coord):
+        """
+        :param Tensor coord: (B, K, 2)
+        """
+        toB = torch.arange(coord.size(0))
+        indices = coord[:, :, 0].argsort()  # sort by x coordinate (B, K)
+        coord = coord[toB.unsqueeze(1), indices]  # (B, K, 2)
+        return coord
+    
+    def _sort_by_yx(self, coord):
+        toB = torch.arange(coord.size(0))
+        indices = coord[:, :, 1].argsort()  # sort by y coordinate
+        coord = coord[toB.unsqueeze(1), indices]
+        return coord
+
+    def forward(self, x):
+        """
+        :param Tensor x: (B, N, 2)
+        :return Tensor final_embedding: (B, N, H)
+        """
+        bsz, seq_len = x.size(0), x.size(1)
+
+        node_embedding = self.W1(x)  # (B, N, H)
+
+        # Make k-NN for each node (B, N, K+1, 2)
+        dist_matrix = torch.cdist(x, x)  # (B, N, N)
+        knn_indices = dist_matrix.topk(k=seq_len)[1][:,:,-self.nb_neighbors-1:]  # (B, N, K+1) including itself
+
+        embedding_list = []
+        for i in range(seq_len):
+            idx = knn_indices[:, i, :].unsqueeze(2).repeat(1,1,2)  # (B, K+1, 2)
+            knn_coords = x.gather(1, idx)
+
+            knn_coords_x = self._sort_by_xy(knn_coords)  # (B, K+1, 2)
+            knn_coords_y = self._sort_by_yx(knn_coords)  # (B, K+1, 2)
+
+            knn_coords_x = knn_coords_x.permute(0, 2, 1)  # (B, 2, K+1)
+            conv_embedding_x = self.conv_x(knn_coords_x)  # (B, H, 1)
+            
+            knn_coords_y = knn_coords_y.permute(0, 2, 1)  # (B, 2, K+1)
+            conv_embedding_y = self.conv_y(knn_coords_y)  # (B, H, 1)
+
+            conv_embedding = conv_embedding_x + conv_embedding_y  # (B, H, 1)
+
+            conv_embedding = conv_embedding.permute(0, 2, 1)  # (B, 1, H)
+            # conv_embedding = self.W2(conv_embedding)  # (B, 1, H)
+
+            embedding_list.append(conv_embedding)
+        conv_embedding = torch.cat(embedding_list, dim=1)  # (B, N, H)
+        conv_embedding = self.W2(conv_embedding)  # (B, N, H)  namely CEFix
+
+        final_embedding = node_embedding + conv_embedding  # (B, N, H)
+        return final_embedding
+     
+def compute_tour_length(x, tour): 
+    """
+    Compute the length of a batch of tours
+    Inputs : x of size (bsz, nb_nodes, 2) batch of tsp tour instances
+             tour of size (bsz, nb_nodes) batch of sequences (node indices) of tsp tours
+    Output : L of size (bsz,)             batch of lengths of each tsp tour
+    """
+    bsz = x.shape[0]
+    nb_nodes = x.shape[1]
+    arange_vec = torch.arange(bsz, device=x.device)
+    first_cities = x[arange_vec, tour[:,0], :] # size(first_cities)=(bsz,2)
+    previous_cities = first_cities
+    L = torch.zeros(bsz, device=x.device)
+    with torch.no_grad():
+        for i in range(1,nb_nodes):
+            current_cities = x[arange_vec, tour[:,i], :] 
+            L += (current_cities - previous_cities).pow(2).sum(dim=1).sqrt()
+            # L += torch.sum( torch.round((current_cities - previous_cities)**2) , dim=1 )**0.5 # dist(current, previous node) 
+            previous_cities = current_cities
+        L += (current_cities - first_cities).pow(2).sum(dim=1).sqrt()
+        # L += torch.sum( torch.round((current_cities - first_cities)**2) , dim=1 )**0.5 # dist(last, first node)  
+    return L
